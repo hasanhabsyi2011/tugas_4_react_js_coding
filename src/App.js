4384b33fb31e6746106182aefb06bff8c6b4bc5e
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-29 19:59:24 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan';
+
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+          <MenuMakanan />
+      </div>
+    );
+  }
+}
+export default App;
